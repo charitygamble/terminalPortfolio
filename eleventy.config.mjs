c6162dd3d0c9c4cab4e31d3ffc0a24ac4395fe93
Charity Gamble <cgamble.dev@proton.me>
2026-03-25 13:11:07 -0600

    Initial Commit

---
+++ eleventy.config.mjs
@@ -0,0 +1,33 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('src/assets');
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addFilter("dateString", (dateObj) => {
+      return dateObj.toISOString().split('T')[0];
+    });
+    eleventyConfig.addCollection("labs", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("src/labs/*.md").reverse();
+    });
+    eleventyConfig.addCollection("projects", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("src/projects/*.md");
+    });
+
+    eleventyConfig.addGlobalData("permalink", () => {
+    return (data) => {
+      return `${data.page.filePathStem}.html`;
+    };
+  });
+    
+    return {
+        dir: {
+            input: 'src',
+            output: 'public',
+            includes: '_includes',
+        }
+    };
+};
