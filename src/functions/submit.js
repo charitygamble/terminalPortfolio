90564bedda8e61cdde20588d7ae46cf18605a7c6
Charity Gamble <cgamble.dev@proton.me>
2026-03-25 18:19:23 -0600

    contact form tweaks

---
+++ src/functions/submit.js
@@ -0,0 +1,20 @@
+export async function onRequestPost(context) {
+  const formData = await context.request.formData();
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const message = formData.get("message");
+
+  // MailChannels logic
+  const res = await fetch("https://api.mailchannels.net/tx/v1/send", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      personalizations: [{ to: [{ email: "charity@charitygamble.dev" }] }],
+      from: { email: "contact@yourdomain.com", name: "Portfolio" },
+      subject: `New Message from ${name}`,
+      content: [{ type: "text/plain", value: message }]
+    })
+  });
+
+  return Response.redirect("https://charitygamble.dev/success", 303);
+}
